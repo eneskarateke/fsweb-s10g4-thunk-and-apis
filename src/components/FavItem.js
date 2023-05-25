3aeef60a99ccd2b4b231c27c6da69e7bd50530b6
eneskarateke <eneskarateke@gmail.com>
2023-05-25 17:06:12 +0300

    tailwind ile birkaç stil eklendi, favori atems

--- src/components/FavItem.js
+++ src/components/FavItem.js
@@ -19,8 +19,8 @@ function FavItem({ activity, id }) {
   };
 
   return (
-    <div className="bg-white shadow hover:shadow-lg p-3 pl-5 flex items-center group transition-all">
-      <div className="flex-1 pr-4">{activity}</div>
+    <div className="bg-white hover:bg-green-200 shadow hover:shadow-lg p-3 pl-5 flex items-center group transition-all">
+      <div className="flex-1  h-6 pr-4 font-bold">{activity}</div>
       <button
         onClick={() => removeFavHandler(id)}
         className="transition-all px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100"
